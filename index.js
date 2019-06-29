3dccbca0a6c00789b83ae7043e013ac9cbff1cb5
Matthew Faden <matthewtfaden@gmail.com>
2019-06-29 16:12:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,3 @@ lowercase.toLowerCase() === lowercase
 function sayHiToGrandma(string) {
 if(string.toLowerCase) return 'I can\'t hear you!'
 }
-function sayHiToGrandma(string) {
-  if(string.toUpperCase) return 'YES INDEED!'
-}
